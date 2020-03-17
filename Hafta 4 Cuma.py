4286c7b3928e04344511449052a3045e91811b67
Egemenatilla <44278787+Egemenatilla@users.noreply.github.com>
2020-03-17 21:36:35 +0300

    Update Hafta 4 Cuma.py

--- Hafta 4 Cuma.py
+++ Hafta 4 Cuma.py
@@ -1,43 +1,21 @@
-from sympy import Symbol
-theta=Symbol('theta')
-
-import math
-math.sin(theta)
-
 import sympy
-sympy.sin(theta)+sympy.sin(theta)
-print(2*sin(theta))
+import math
+from sympy import Symbol, solve, sin, Limit, S, oo)
+theta = Symbol('theta')# math.sin(theta) 
 
-print("----------------------------")
+print(sympy.sin(theta)*sympy.sin(theta))
+u = Symbol('u')
+t = Symbol('t')
+g = Symbol('g') 
+print(solve(u*sin(theta)-g*t, t)) 
 
-u=Symbol('u')
-t=Symbol('t')
-g=Symbol('g')
+x = Symbol('x')
+l = Limit(1/x, x, oo)print(l.doit()) 
+st = 5*t**2 + 2*t + 8
+t1 = Symbol('t1')
+delta_t = Symbol('delta_t')
+st1 = st.subs({t:t1}) 
 
-theta=Symbol('theta')
-
-from sympy import solve,sin
-
-solve(u*sin(theta)-g*t,t)
-print("------------------------------")
-
-x=Symbol('x')
-from sympy import Limit,S
-l=Limit(1/x,x,S,dir='-') #limit,s burada sonsuzluk belirtir.
-print(l)
-#l.doit()
-
-print("--------------------------------")
-t=Symbol('t')
-st=5*t**2+2*t+8
-
-t1=Symbol('t1')
-delta_t=Symbol('delta_t')
-
-st1=st.subs({t:t1})
-
-st1_delta=(st.subs({t:t1+delta_t}))
-
-f_d=Limit((st1_delta-st1)/delta_t,0)
-print(f_d)
-#f_d.doit()
+st1_delta = (st.subs({t: t1+delta_t}))
+f_d = Limit(((st1_delta - st1) / delta_t), delta_t, 0) 
+print(f_d.doit())
